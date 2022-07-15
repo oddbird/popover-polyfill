005473e052eee6f55b562f2d70087c05c69afcf8
Keith Cirkel <keithamus@users.noreply.github.com>
2022-07-15 09:56:04 +0100

    switch to using ids for all popup tests

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -18,32 +18,32 @@ expect.extend({
 })
 
 test('popup as a boolean attribute', async ({ page }) => {
-  const popup = (await page.locator('#popups [popup]')).nth(0)
+  const popup = (await page.locator('#popup1')).nth(0)
   await expect(popup).toBeFunctionalPopup()
 })
 
 test('popup as ""', async ({ page }) => {
-  const popup = (await page.locator('#popups [popup=""]')).nth(0)
+  const popup = (await page.locator('#popup2')).nth(0)
   await expect(popup).toBeFunctionalPopup()
 })
 
 test('popup as "auto"', async ({ page }) => {
-  const popup = (await page.locator('#popups [popup=auto]')).nth(0)
+  const popup = (await page.locator('#popup3')).nth(0)
   await expect(popup).toBeFunctionalPopup()
 })
 
 test('popup as "hint"', async ({ page }) => {
-  const popup = (await page.locator('#popups [popup=hint]')).nth(0)
+  const popup = (await page.locator('#popup4')).nth(0)
   await expect(popup).toBeFunctionalPopup()
 })
 
 test('popup as "manual"', async ({ page }) => {
-  const popup = (await page.locator('#popups [popup=manual]')).nth(0)
+  const popup = (await page.locator('#popup5')).nth(0)
   await expect(popup).toBeFunctionalPopup()
 })
 
 test('popup as "invalid"', async ({ page }) => {
-  const popup = (await page.locator('#popups [popup=invalid]')).nth(0)
+  const popup = (await page.locator('#popup6')).nth(0)
   await expect(popup).toBeVisible()
   await expect(async () => await popup.evaluate(node => node.showPopUp()))
     .rejects.toThrow('DOMException: Not supported on element that does not have valid popup attribute')
