bb22ebd23b5a7416536fb253e535c5af510efa94
Keith Cirkel <keithamus@users.noreply.github.com>
2022-09-22 18:09:15 +0100

    format all files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import {isSupported, apply} from './popup.js'
+import { apply, isSupported } from './popup.js';
 
-if (!isSupported()) apply()
+if (!isSupported()) apply();
 
 declare global {
   interface HTMLElement {
-    popUp: 'auto' | 'hint' | 'manual'
-    defaultOpen: boolean
-    showPopUp(): void
-    hidePopUp(): void
+    popUp: 'auto' | 'hint' | 'manual';
+    defaultOpen: boolean;
+    showPopUp(): void;
+    hidePopUp(): void;
   }
 }
