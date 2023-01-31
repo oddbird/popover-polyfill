f41c7f7e5ecea027caa66104fe04ce7ce9efe9a8
yinonov <yinon.oved@vonage.com>
2023-01-31 11:54:26 +0200

    reduce change

--- src/popover.ts
+++ src/popover.ts
@@ -27,20 +27,6 @@ const closestAncestor: (selector: string, target: Element) => Element | null = (
   return closestAncestor(selector, root.host);
 };
 
-const queryAncestorAll = (
-  element: Element,
-  selector: string,
-  popovers: Element[] = [],
-): Element[] => {
-  // there could be multiple popovers nested inside each other
-  const ancestor = closestAncestor(selector, element);
-  const parent =
-    ancestor?.parentElement || (ancestor?.getRootNode() as ShadowRoot)?.host;
-  return ancestor && parent
-    ? queryAncestorAll(parent, selector, [ancestor, ...popovers])
-    : popovers;
-};
-
 function patchAttachShadow(callback: (shadowRoot: ShadowRoot) => void) {
   const originalAttachShadow = Element.prototype.attachShadow;
   Element.prototype.attachShadow = function (init) {
