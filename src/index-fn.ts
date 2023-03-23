1238bb79c02354155decead1bb37603f18bfe347
Jonny Gerig Meyer <jonny@oddbird.net>
2023-03-23 10:26:05 -0400

    Add types to fn export

--- src/index-fn.ts
+++ src/index-fn.ts
@@ -1,8 +1,25 @@
+interface PopoverToggleTargetElementInvoker {
+  popoverTargetElement: HTMLElement | null;
+  popoverTargetAction: 'toggle' | 'show' | 'hide';
+}
+
 declare global {
+  interface ToggleEvent extends Event {
+    oldState: string;
+    newState: string;
+  }
   interface HTMLElement {
     popover: 'auto' | 'manual' | null;
     showPopover(): void;
     hidePopover(): void;
+    togglePopover(): void;
+  }
+  /* eslint-disable @typescript-eslint/no-empty-interface */
+  interface HTMLButtonElement extends PopoverToggleTargetElementInvoker {}
+  interface HTMLInputElement extends PopoverToggleTargetElementInvoker {}
+  /* eslint-enable @typescript-eslint/no-empty-interface */
+  interface Window {
+    ToggleEvent: ToggleEvent;
   }
 }
 
